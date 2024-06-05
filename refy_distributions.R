83b75127d71f1d94140a7d9a9d2e1a47b40ae9d9
zander-prinsloo <prinsloo.zander@gmail.com>
2024-06-05 18:01:30 -0400

    clean and add useful comments

--- refy_distributions.R
+++ refy_distributions.R
@@ -1,14 +1,3 @@
-dt_ref <- refy(gls = gls,
-               dsm = dsm,
-               pinv = pinv,
-               dl_aux = dl_aux) |>
-  refy_mult_factor()
-
-
-dt_ref |> dim()
-
-rm <- dt_ref
-
 
 refy_distributions <- function(rm, cntry_code, ref_year, gls) {
 
@@ -18,50 +7,51 @@ refy_distributions <- function(rm, cntry_code, ref_year, gls) {
     fsubset(country_code == cntry_code &
               reporting_year == ref_year)
 
-  # cache ids
+  # Load surveys
   cache_id <- rm$cache_id
-
-  # # survey years
-  # svys <- pipload::pip_find_cache(cntry_code,
-  #                                 version = gls$vintage_dir)
-  # yrs  <- extract_year(svys)
-  # keep <- which(abs(yrs - refy) <= minN(x = abs(yrs - refy),
-  #                                       N = 2))
-  # yrs  <- yrs[keep]
-
-  # reporting level change
-  # welfare level change?
-
-
-  # Interpolation vs extrapolation
-  # if ( all(yrs <= refy)) {
-  #   yrs <- yrs[which.min(abs(yrs - refy))]
-  # }
-
-  # Append (possibly two) surveys on other side of refy
   df_svy <- collapse::rowbind(lapply(as.list(cache_id),
                                       FUN = function(x){
                                         pipload::pip_load_cache(cache_id = x,
                                                                 version = gls$vintage_dir) |>
                                           fselect(country_code, surveyid_year, survey_acronym,
-                                                  survey_year, welfare, weight, reporting_level, welfare_type)
+                                                  survey_year, welfare_ppp, weight,
+                                                  reporting_level, welfare_type, imputation_id)
                                       }))
-  # adjust weights - there are two separate surveys now with incomparable weights
-  df_svy <- df_svy |>
-    fgroup_by(survey_year, reporting_level, welfare_type) |>
-    fmutate(weight = weight / fsum(weight)) |>
-    fungroup()
 
+  # Join welfare & weights vectors from surveys to rm
   df_refy <- rm |>
-    fsubset(country_code == cntry_code) |>
-    joyn::joyn(y          = df_svy,
-               by         = c("country_code", "reporting_level", "welfare_type", "survey_year"),
-               keep       = "left",
-               match_type = "1:m",
-               verbose = T) |>
-    fmutate(welfare_lineup = welfare * mult_factor)
+    joyn(y          = df_svy,
+         by         = c("country_code",
+                        "reporting_level",
+                        "welfare_type",
+                        "survey_year"),
+         keep       = "left",
+         match_type = "1:m",
+         verbose    = TRUE,
+         reportvar  = FALSE) |>
+    # Group by survey year
+    fgroup_by(survey_year) |>
+    # number of imputations per survey year (if micro data then n_imp = 1)
+    fmutate(n_imp      = data.table::uniqueN(imputation_id),
+            # population at survey (decimal) year found by summing survey weights
+            svy_pop    = fsum(weight),
+            # weights scaled by ratio of ref year and svy year pop
+            #         to make relative to reference year
+            # weights adj by rel dist to get weighted average of population at ref year
+            frq_weight = weight * (reporting_pop / svy_pop) * # "adjust to WDI population" --> Andres, your comment
+              relative_distance,
+            # ref year weights divided by number of imputations
+            #      this should sum to poplution amount
+            weight_refy = frq_weight / n_imp) |>
+    fungroup() |>
+    fmutate(welfare = welfare * mult_factor)
 
   df_refy
 
 }
+
+
+
+
+
 
