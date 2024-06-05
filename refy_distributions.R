fd50e9a0db211845d4024a6ed45600921371345a
zander-prinsloo <prinsloo.zander@gmail.com>
2024-06-05 17:47:44 -0400

    refy distributions w commented code

--- refy_distributions.R
+++ refy_distributions.R
@@ -1,45 +1,67 @@
-refy_distributions <- function(rm, cntry_code, refy, gls) {
-  # for now
-  country_code <- "USA"
-  refy <- 2017
+dt_ref <- refy(gls = gls,
+               dsm = dsm,
+               pinv = pinv,
+               dl_aux = dl_aux) |>
+  refy_mult_factor()
 
-  # survey years
-  svys <- pipload::pip_find_cache(country_code,
-                                  version = gls$vintage_dir)
-  yrs  <- extract_year(svys)
-  keep <- which(abs(yrs - refy) <= minN(x = abs(yrs - refy),
-                                        N = 2))
-  yrs  <- yrs[keep]
+
+dt_ref |> dim()
+
+rm <- dt_ref
+
+
+refy_distributions <- function(rm, cntry_code, ref_year, gls) {
+
+  # Filter rm
+  rm <-
+    rm |>
+    fsubset(country_code == cntry_code &
+              reporting_year == ref_year)
+
+  # cache ids
+  cache_id <- rm$cache_id
+
+  # # survey years
+  # svys <- pipload::pip_find_cache(cntry_code,
+  #                                 version = gls$vintage_dir)
+  # yrs  <- extract_year(svys)
+  # keep <- which(abs(yrs - refy) <= minN(x = abs(yrs - refy),
+  #                                       N = 2))
+  # yrs  <- yrs[keep]
+
+  # reporting level change
+  # welfare level change?
+
 
   # Interpolation vs extrapolation
-  if ( all(yrs <= refy)) {
-    yrs <- yrs[which.min(abs(yrs - refy))]
-  }
+  # if ( all(yrs <= refy)) {
+  #   yrs <- yrs[which.min(abs(yrs - refy))]
+  # }
 
   # Append (possibly two) surveys on other side of refy
-  df_svy <- collapse::rowbind(lapply(as.list(yrs),
+  df_svy <- collapse::rowbind(lapply(as.list(cache_id),
                                       FUN = function(x){
-                                        pipload::pip_load_cache(cntry_code,
-                                                                x,
+                                        pipload::pip_load_cache(cache_id = x,
                                                                 version = gls$vintage_dir) |>
                                           fselect(country_code, surveyid_year, survey_acronym,
                                                   survey_year, welfare, weight, reporting_level, welfare_type)
                                       }))
   # adjust weights - there are two separate surveys now with incomparable weights
   df_svy <- df_svy |>
-    fgroup_by(survey_year) |>
-    fmutate(weight = weight / fsum(weight,
-                                   g = survey_year))
+    fgroup_by(survey_year, reporting_level, welfare_type) |>
+    fmutate(weight = weight / fsum(weight)) |>
+    fungroup()
 
   df_refy <- rm |>
     fsubset(country_code == cntry_code) |>
     joyn::joyn(y          = df_svy,
                by         = c("country_code", "reporting_level", "welfare_type", "survey_year"),
                keep       = "left",
-               match_type = "m:m",
+               match_type = "1:m",
                verbose = T) |>
     fmutate(welfare_lineup = welfare * mult_factor)
 
   df_refy
 
 }
+
