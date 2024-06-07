05e9043877e9f5bc9e00dfb742d1d538a31fd8f7
R.Andres Castaneda <acastastanedaa@worldbank.org>
2024-06-07 12:07:48 -0400

    min formatting to read the function better

--- R/refy_mult_factor.R
+++ R/refy_mult_factor.R
@@ -1,13 +1,23 @@
 refy_mult_factor <- function(df_refy) {
 
   df_refy |>
-    fmutate(lineup_approach = fcase(estimation_type == "extrapolation" , "extrapolation",
-                                    estimation_type == "interpolation" & monotonic == TRUE & same_direction == TRUE, "interpolation_same",
-                                    estimation_type == "interpolation" & !(monotonic == TRUE & same_direction == TRUE), "interpolation_diverge",
-                                    default = "survey"),
-            mult_factor = fcase(lineup_approach == "extrapolation" | lineup_approach == "interpolation_diverge", nac/nac_sy,
-                                lineup_approach == "interpolation_same", predicted_mean_ppp/svy_mean,
-                                default = 1))
+    fmutate(
+      lineup_approach = fcase(
+        estimation_type == "extrapolation" ,
+        "extrapolation",
+        estimation_type == "interpolation" & monotonic == TRUE & same_direction == TRUE, "interpolation_same",
+        estimation_type == "interpolation" & !(monotonic == TRUE & same_direction == TRUE),
+        "interpolation_diverge",
+        default = "survey"
+      ),
+      mult_factor = fcase(
+        lineup_approach == "extrapolation" | lineup_approach == "interpolation_diverge",
+        nac / nac_sy,
+        lineup_approach == "interpolation_same",
+        predicted_mean_ppp / svy_mean,
+        default = 1
+      )
+    )
 
 }
 
