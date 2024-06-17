7a34e98dbb1d33d28c4305881ebbbf565d406379
zander-prinsloo <prinsloo.zander@gmail.com>
2024-06-17 18:36:47 -0400

    reduce refy distribution data frame

--- R/refy_distributions.R
+++ R/refy_distributions.R
@@ -7,6 +7,35 @@ refy_distributions <- function(rm, cntry_code, ref_year, gls) {
     fsubset(country_code == cntry_code &
               reporting_year == ref_year)
 
+  # reduce rm
+  rm <- rm |>
+    vars_to_attr(vars = c("income_group_code",
+                          "reporting_year",
+                          "nac",
+                          "estimation_type",
+                          "lineup_case",
+                          "interpolation_id",
+                          "reporting_gdp",
+                          "reporting_pce",
+                          "monotonic",
+                          "same_direction",
+                          "wb_region_code",
+                          "pcn_region_code",
+                          "survey_coverage",
+                          "survey_comparability",
+                          "comparable_spell",
+                          "ppp",
+                          "pop_data_level",
+                          "gdp_data_level",
+                          "pce_data_level",
+                          "cpi_data_level",
+                          "ppp_data_level",
+                          "is_interpolated",
+                          "is_used_for_line_up",
+                          "is_used_for_aggregation",
+                          "display_cp",
+                          "lineup_approach"))
+
   # Load surveys
   cache_id <- rm$cache_id
   df_svy <- collapse::rowbind(lapply(as.list(cache_id),
@@ -49,6 +78,15 @@ refy_distributions <- function(rm, cntry_code, ref_year, gls) {
   # temp
   setkey(df_refy, NULL)
 
+  # keep attributes of rm
+  attributes(df_refy) <- c(attributes(df_refy),
+                           attributes(rm)[-which(names(attributes(rm)) %in%
+                                                   c("dim",
+                                                     "row.names",
+                                                     "names",
+                                                     "class",
+                                                     ".internal.selfref",
+                                                     names(attributes(df_refy))))])
   df_refy
 
 }
