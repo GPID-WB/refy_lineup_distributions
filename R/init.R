96de696bbfd18e9cf2f7e7d0e221728e5c48cc51
zander-prinsloo <prinsloo.zander@gmail.com>
2024-06-21 14:48:49 -0400

    load scripts and paths

--- R/init.R
+++ R/init.R
@@ -1,9 +1,10 @@
 # Load packages
-config   <-  config::get(config = Sys.info()['user'])
-base_dir <- config$base_dir
+base_dir <- fs::path("P:/02.personal/wb612474/pip-technical/pip_ingestion_pipeline")
+#config   <-  config::get(config = Sys.info()['user'])
+#base_dir <- config$base_dir
 withr::with_dir(new = base_dir,
                 code = {
-                  # source("./_packages.R")
+                   #source("./_packages.R")
 
                   # Load R files
                   purrr::walk(fs::dir_ls(path = "./R",
@@ -16,14 +17,33 @@ withr::with_dir(new = base_dir,
 
                 })
 
+
+output_dir <- "P:/03.pip/lineup_distributions/"
+output_dir_refy <- fs::path(output_dir,
+                            "output-lineup-ref-years")
+
 # Load utils functions
-source("R/utils.R")
+# source("R/utils.R")
+#devtools::install_github(repo = "PIP-Technical-Team/pipdata@DEV")
+
+#devtools::install_github(repo = "PIP-Technical-Team/pipload@DEV")
+
+all_scripts <- list.files(fs::path("R"),
+                          pattern="*.R$", full.names=TRUE,
+                          ignore.case=TRUE)
+
+
+sapply(all_scripts[-which(all_scripts == "R/init.R")],
+       source,
+       .GlobalEnv)
+
 library(collapse)
 library(data.table)
 library(joyn)
 library(qs)
+library(pipdata)
 # Load objects
-# gls    <- qs::qread(file = fs::path("data_inputs/gls.qs"))
-# dsm    <- qs::qread(file = fs::path("data_inputs/dsm.qs"))
-# pinv   <- qs::qread(file = fs::path("data_inputs/pinv.qs"))
-# dl_aux <- qs::qread(file = fs::path("data_inputs/dl_aux.qs"))
+gls    <- qs::qread(file = fs::path("data_inputs/gls.qs"))
+dsm    <- qs::qread(file = fs::path("data_inputs/dsm.qs"))
+pinv   <- qs::qread(file = fs::path("data_inputs/pinv.qs"))
+dl_aux <- qs::qread(file = fs::path("data_inputs/dl_aux.qs"))
