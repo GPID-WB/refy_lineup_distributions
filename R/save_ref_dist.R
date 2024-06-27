76ddcf88902f396bf9e1cd7383e332746965e7c2
zander-prinsloo <prinsloo.zander@gmail.com>
2024-06-27 15:25:23 -0400

    standard attributes, all in one script

--- R/save_ref_dist.R
+++ R/save_ref_dist.R
@@ -7,93 +7,84 @@
 #' @return invisible logical - purpose is to save file
 #' @keywords internal
 save_ref_dist <- function(df_refy,
-                          path) {
+                          path,
+                          inc_svy_year = FALSE) {
 
-  #is_interpolated <- attributes(df_refy)$is_interpolated
-  length_uniq_var <- length(attributes(df_refy)$interpolation_id)
-  n_imp           <- attributes(df_refy)$n_imp
-  # print(list(l = length_uniq_var,
-  #            n_imp = n_imp))
-  if (length(n_imp) > 1) {
-    any_imp <- any(df_refy$distribution_type == "imputed")
-    n_imp   <- 2 # multiple imputation (e.g. NGA) doesn't have unique numvars
-  }
+  # length_uniq_var <- length(attributes(df_refy)$interpolation_id)
+  # n_imp           <- attributes(df_refy)$n_imp
+  #
+  # if (length(n_imp) > 1) {
+  #   any_imp <- any(df_refy$distribution_type == "imputed")
+  #   n_imp   <- 2 # multiple imputation (e.g. NGA) doesn't have unique numvars
+  # }
 
-  #print(attributes(df_refy))
-  #print(n_imp)
-  if (length_uniq_var == 1 &
-      n_imp == 1
-      #(!is_interpolated & length_uniq_var == 1)
-      ) {
+  # if (length_uniq_var == 1 &
+  #     n_imp == 1) {
+  #
+  #   print("one")
 
-    print("one")
+
     # some specific countries (e.g. CHN) produce
     #  "Error in `vars_to_list()`:
     #    ! The unique values in `num_var` and `name_var` column
     #       are not equal"
     # For CHN: because rural and urban have different means (svy_mean)
     #   etc. for the same survey year
-    numvars <- c("nac_sy",
-                 "svy_mean",
-                 "survey_id",
-                 "cache_id",
-                 "surveyid_year",
-                 #"survey_time", # SYR 2007 doesn't have `survey_time`
-                 "survey_mean_lcu",
-                 "survey_mean_ppp",
-                 "cpi",
-                 "mult_factor",
-                 "svy_pop")
+
+    #numvars <- c()
 
     # vars as attributes
+  if (isFALSE(inc_svy_year)) {
     df_refy <- df_refy |>
       vars_to_attr(var = c("country_code",
-                           "reporting_level",
-                           "welfare_type",
-                           "reporting_pop",
-                           "distribution_type",
-                           "gd_type",
-                           "relative_distance"#,
-                           #"n_imp"
-                           )) |>
-      # per survey year
-      num_vars_to_attr(num_var = numvars,
-                       name_var = rep("survey_year",
-                                      length(numvars)))
-
+                           "survey_year",
+                           "survey_acronym"
+      ))
   } else {
-    print("two")
-    # if error above, then reduce number of `numvars`
-    #  that can be made attributes
-    numvars <- c("nac_sy",
-                 #"svy_mean",
-                 "survey_id",
-                 "cache_id",
-                 "surveyid_year",
-                 "survey_time",
-                 #"survey_mean_lcu",
-                 #"survey_mean_ppp",
-                 #"cpi",
-                 #"mult_factor",
-                 "svy_pop")
-
-    # vars as attributes
     df_refy <- df_refy |>
       vars_to_attr(var = c("country_code",
-                           "reporting_level",
-                           "welfare_type",
-                           "reporting_pop",
-                           "distribution_type",
-                           "gd_type",
-                           "relative_distance"
-                           #"n_imp"
-                           )) |>
-      # per survey year
-      num_vars_to_attr(num_var  = numvars,
-                       name_var = rep("survey_year",
-                                      length(numvars)))
-
+                           "survey_acronym"
+      ))
   }
+#|>
+      # per survey year
+      # num_vars_to_attr(num_var = numvars,
+      #                  name_var = rep("survey_year",
+      #                                 length(numvars)))
+
+  # } else {
+  #   print("two")
+  #   # if error above, then reduce number of `numvars`
+  #   #  that can be made attributes
+  #   numvars <- c("nac_sy",
+  #                #"svy_mean",
+  #                "survey_id",
+  #                "cache_id",
+  #                "surveyid_year",
+  #                "survey_time",
+  #                #"survey_mean_lcu",
+  #                #"survey_mean_ppp",
+  #                #"cpi",
+  #                #"mult_factor",
+  #                "svy_pop")
+  #
+  #   # vars as attributes
+  #   df_refy <- df_refy |>
+  #     vars_to_attr(var = c("country_code",
+  #                          #"reporting_level",
+  #                          #"welfare_type",
+  #                          "reporting_pop",
+  #                          #"distribution_type",
+  #                          "gd_type",
+  #                          "relative_distance"
+  #                          #"n_imp"
+  #                          )) |>
+  #     # per survey year
+  #     num_vars_to_attr(num_var  = numvars,
+  #                      name_var = rep("survey_year",
+  #                                     length(numvars)))
+  #
+  # }
 
   cntry_code <- attributes(df_refy)$country_code
   ref_year   <- attributes(df_refy)$reporting_year
@@ -135,6 +126,51 @@ apply_save_ref_dist <- function(list_refy,
 
 
 
+#' Estimate and save the reference year distributions for all countries-ref-years
+#'
+#' A wrapper over [refy_distributions]  and [save_ref_dist]
+#' applied on a list of data frames
+#'
+#' @param df_refy Output from [refy_distributions]
+#' @param cntry_refy list: each element is another list containing
+#'                   country_code scalar (e.g. "ZAF") and year vector
+#'                   (e.g.`c(2001, 2002, 2003)`). If there are four countries,
+#'                   the `length(cntry_refy) = 4`, one for each country with its
+#'                   year vector.
+#' @param path path to save the output - "P:\03.pip\lineup_distributions\output-lineup-ref-years"
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' full_refy_estimate_save(df_refy = dt_ref,
+#'                         cntry_refy = list(list(country_code = "ZAF",
+#'                                                year         = 2000:2005),
+#'                                           list(country_code = "COL",
+#'                                                year         = 2000:2005)),
+#'                         path = output_dir_refy)
+full_refy_estimate_save <- function(df_refy, cntry_refy, path, gls, inc_svy_year = FALSE) {
+
+  lapply(cntry_refy,
+         FUN = \(x) {
+
+           lapply(x$year,
+                  FUN = \(year = x$year,
+                          country_code = x$country_code){
+
+                    suppressMessages(refy_distributions(rm         = df_refy,
+                                                        cntry_code = country_code,
+                                                        ref_year   = year,
+                                                        gls        = gls) |>
+                                       save_ref_dist(path = path,
+                                                     inc_svy_year = inc_svy_year))
+
+                    print(paste0(country_code, "_", year))
+                  }
+           )
+         })
+  invisible(TRUE)
+}
 
 
 
